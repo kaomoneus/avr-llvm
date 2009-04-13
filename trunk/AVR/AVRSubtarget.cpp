dca79ec230847901fa108669b0708ae70751bc75
zapster <zapster@ddab61f3-e77b-45cd-8477-85da285f4094>
2009-04-13 16:47:29 +0000

    - AVR Target added
    git-svn-id: svn://svn.code.sf.net/p/avr-llvm/code@3 ddab61f3-e77b-45cd-8477-85da285f4094

---
+++ trunk/AVR/AVRSubtarget.cpp
@@ -0,0 +1,25 @@
+//===- AVRSubtarget.cpp - AVR Subtarget Information -----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source 
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the AVR specific subclass of TargetSubtarget.
+//
+//===----------------------------------------------------------------------===//
+
+#include "AVRSubtarget.h"
+#include "AVRGenSubtarget.inc"
+
+using namespace llvm;
+
+AVRSubtarget::AVRSubtarget(const Module &M, const std::string &FS) {
+  std::string CPU = "generic";
+  
+
+  // Parse features string.
+  ParseSubtargetFeatures(FS, CPU);
+}
