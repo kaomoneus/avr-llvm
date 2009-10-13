bbdcf96b229af6a75eceb8cbb8c2f2a0c0f0a932
jmyers <jmyers@ddab61f3-e77b-45cd-8477-85da285f4094>
2009-10-13 23:20:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/avr-llvm/code@19 ddab61f3-e77b-45cd-8477-85da285f4094

---
+++ branches/release_26/AVR/AVRFrameInfo.h
@@ -0,0 +1,33 @@
+//===-- AVRTargetFrameInfo.h - Define TargetFrameInfo for AVR ---*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef AVR_FRAMEINFO_H
+#define AVR_FRAMEINFO_H
+
+#include "AVR.h"
+#include "AVRSubtarget.h"
+#include "llvm/Target/TargetFrameInfo.h"
+
+
+namespace llvm {
+
+class AVRFrameInfo : public TargetFrameInfo {
+
+  public:
+    AVRFrameInfo(const TargetMachine &tm)
+		:  TargetFrameInfo(StackGrowsDown, 1, 0) {}
+};
+
+} // End llvm namespace
+
+#endif
--- branches/release_26/AVR/AVRISelDAGToDAG.cpp
+++ branches/release_26/AVR/AVRISelDAGToDAG.cpp
@@ -2,7 +2,7 @@
 //
 //                     The LLVM Compiler Infrastructure
 //
-// This file is distributed under the University of Illinois Open Source 
+// This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
@@ -13,18 +13,52 @@
 
 #define DEBUG_TYPE "avr-isel"
 
-#include "AVRISelDAGToDAG.h"
+//#include "AVRISelDAGToDAG.h"
+#include "AVR.h"
+#include "AVRISelLowering.h"
+#include "AVRTargetMachine.h"
+//#include "llvm/CallingConv.h"
+//#include "llvm/Constants.h"
+//#include "llvm/DerivedTypes.h"
+#include "llvm/Function.h"
+//#include "llvm/Intrinsics.h"
+//#include "llvm/LLVMContext.h"
+//#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/SelectionDAG.h"
+#include "llvm/CodeGen/SelectionDAGISel.h"
+//#include "llvm/Target/TargetLowering.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/Support/Compiler.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
 
 using namespace llvm;
 
+class AVRDAGToDAGISel : public SelectionDAGISel {
+  AVRTargetMachine &TM;
+
+  /// Subtarget - Keep a pointer to the ARMSubtarget around so that we can
+  /// make the right decision when generating code for different targets.
+  const AVRSubtarget *Subtarget;
+
+public:
+  explicit AVRDAGToDAGISel(AVRTargetMachine &tm)
+    : SelectionDAGISel(tm), TM(tm),
+    Subtarget(&TM.getSubtarget<AVRSubtarget>()) {
+  }
+};
+/*
 /// createAVRISelDag - This pass converts a legalized DAG into a
 /// AVR-specific DAG, ready for instruction scheduling.
 FunctionPass *llvm::createAVRISelDag(AVRTargetMachine &TM) {
   return new AVRDAGToDAGISel(TM);
 }
-
-
+*/
+#if 0
 /// InstructionSelect - This callback is invoked by
 /// SelectionDAGISel when it has created a SelectionDAG for us to codegen.
 void AVRDAGToDAGISel::InstructionSelect() {
@@ -44,9 +78,9 @@ SDNode* AVRDAGToDAGISel::Select(SDValue N) {
 }
 
 
-// SelectIOAddr - Match a IO address for DAG. 
+// SelectIOAddr - Match a IO address for DAG.
 // A direct address could be a globaladdress or externalsymbol.
-bool AVRDAGToDAGISel::SelectIOAddr(SDValue Op, SDValue N, 
+bool AVRDAGToDAGISel::SelectIOAddr(SDValue Op, SDValue N,
                                       SDValue &Address) {
   // Return true if TGA or ES.
   if (N.getOpcode() == ISD::TargetGlobalAddress
@@ -57,3 +91,4 @@ bool AVRDAGToDAGISel::SelectIOAddr(SDValue Op, SDValue N,
 
   return false;
 }
+#endif
