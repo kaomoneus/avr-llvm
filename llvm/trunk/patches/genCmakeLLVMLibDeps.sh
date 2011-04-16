b658a22c30eae34f48465d12983100c38d5e3e9c
atomicdog407 <atomicdog407@ddab61f3-e77b-45cd-8477-85da285f4094>
2011-04-16 22:14:06 +0000

    Update patch files
    git-svn-id: svn://svn.code.sf.net/p/avr-llvm/code@113 ddab61f3-e77b-45cd-8477-85da285f4094

--- llvm/trunk/patches/genCmakeLLVMLibDeps.sh
+++ llvm/trunk/patches/genCmakeLLVMLibDeps.sh
@@ -3,7 +3,7 @@
 # run script from inside root
 # Project needs to be build first with all targets enabled using ./configure
 # Creates file LLVMLibDeps.cmake.replace
-# sed script from comments at bottom of llvm/cmake/modules/LLVMConfig.cmake
+# sed script from comments at bottom of llvm/cmake/modules/LLVM-Config.cmake
 
 ./utils/UpdateCMakeLists.pl
 sed -e s'#\.a##g' -e 's#libLLVM#LLVM#g' -e 's#: # #' -e 's#\(.*\)#set(MSVC_LIB_DEPS_\1)#' tools/llvm-config/LibDeps.txt > LLVMLibDeps.cmake.replace
