d7345353304a7d21629402de2e0bee440d9e2638
Stepan Dyatkovskiy <stpworld_dog_narod.ru>
2013-06-11 14:24:09 +0400

    Clang frondend and dragonegg has been removed, since these changes will be tracked in separated repos.

--- cfe/trunk/patches/tryPatch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-patch --dry-run -p0 -i ${DIR}/Targets.diff
-patch --dry-run -p0 -i ${DIR}/interrupt.diff
-patch --dry-run -p0 -i ${DIR}/flash.diff
-
